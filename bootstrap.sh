59d315f9e27757838b1694f78fb389f2082fffc4
waristonpereira <waristonfernando@gmail.com>
2017-09-11 14:52:56 -0300

    Update platforms
    - Remove old SDK
    - Grade up to 4.1
    - Platforms up to 25
    - Minor fix perms

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,34 +1,34 @@
 #!/usr/bin/env bash
 
-ANDROID_SDK_FILENAME=android-sdk_r23.0.1-linux.tgz
-ANDROID_SDK=http://dl.google.com/android/$ANDROID_SDK_FILENAME
+#ANDROID_SDK_FILENAME=android-sdk_r23.0.1-linux.tgz
+#ANDROID_SDK=http://dl.google.com/android/$ANDROID_SDK_FILENAME
 
 apt-get update
 apt-get install -y openjdk-8-jdk openjdk-8-jre lib32stdc++6 lib32z1 expect unzip
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 apt-get install -y nodejs
 npm install -g ionic cordova
-curl -O $ANDROID_SDK
-tar -xzf $ANDROID_SDK_FILENAME -C /home/ubuntu/
-sudo npm config set bin-links false -g
+#curl -O $ANDROID_SDK
+#tar -xzf $ANDROID_SDK_FILENAME -C /home/ubuntu/
+npm config set bin-links false -g
 
-sudo mkdir /opt/gradle
-sudo wget https://services.gradle.org/distributions/gradle-3.4.1-bin.zip
-sudo unzip -d /opt/gradle gradle-3.4.1-bin.zip
+mkdir /opt/gradle
+wget https://services.gradle.org/distributions/gradle-4.1-bin.zip
+unzip -d /opt/gradle gradle-4.1-bin.zip
 
-echo "PATH=\$PATH:/opt/gradle/gradle-3.4.1/bin" >> /home/ubuntu/.bashrc
+echo "PATH=\$PATH:/opt/gradle/gradle-4.1/bin" >> /home/ubuntu/.bashrc
 echo "ANDROID_HOME=~/android-sdk-linux" >> /home/ubuntu/.bashrc
 echo "PATH=\$PATH:~/android-sdk-linux/tools:~/android-sdk-linux/platform-tools" >> /home/ubuntu/.bashrc
 
 mkdir /home/ubuntu/.android/
-touch /home/ubuntu/.android/repositories.cfg
+#touch /home/ubuntu/.android/repositories.cfg
 
 wget --quiet --output-document=tools.zip https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip
 unzip -oqq tools.zip -d /home/ubuntu/android-sdk-linux
 
 expect -c '
 set timeout -1   ;
-spawn /home/ubuntu/android-sdk-linux/tools/bin/sdkmanager "platform-tools" "platforms;android-22" "build-tools;22.0.1" --sdk_root=/home/ubuntu/android-sdk-linux/ 
+spawn /home/ubuntu/android-sdk-linux/tools/bin/sdkmanager "platform-tools" "platforms;android-25" "build-tools;26.0.1" --sdk_root=/home/ubuntu/android-sdk-linux/ 
 expect {
     "ccept" { exp_send "y\r" ; exp_continue }
     eof
@@ -42,3 +42,4 @@ expect {
     eof
 }
 '
+chown ubuntu:ubuntu -R /home/ubuntu/ 
